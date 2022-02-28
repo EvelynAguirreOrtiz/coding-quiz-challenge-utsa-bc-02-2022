199e2d4dab84a6458b3d3fcd0c696dcf68f7b711
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-02-28 14:54:30 -0600

    resolve timer issues

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,75 +1,38 @@
-var startQuizEl = document.querySelector('#start-button');
-var countdownEl = document.getElementById('#countdown');
+var startQuizEl = document.getElementById('start-game');
+var countdownEl = document.getElementById('countdown');
 var questionEl = document.getElementsByClassName('questions')
 
 
-// CLICK TO START TIMER
-
-startQuizEl.addEventListener('click', function() {
-   var timeLeft = 60;
-   console.log(timeLeft);
-
-   function countdownTimer() {
-   //   var timeLeft = 60
-      if (timeLeft = 60) {
-      var timeInterval = setInterval(function() {
-         timeLeft--; 
-         countdownEl.textContent = timeLeft;
-         if (timeLeft = 0) { 
-         clearInterval(timeInterval); 
-         }
-      }, 1000);
-   }};   
-   countdownTimer();
-console.log("timer is counting");
-  
-});
-
-
-
-
-// 
-// function countdownTimer() {
-// 
-//  var timeInterval = setInterval(function() {
-   // timeLeft--; 
-   // countdownEl.textContent = timeLeft;
-   // if (timeLeft === 0) {
-   // clearInterval(timeInterval);       
-   // }}, 1000);
-// };
-//   
-//countdownTimer();
-
-
+// CLICK TO START GAME
 
 // COUNTDOWN TIMER
- 
-// var timeLeft = ""
-// startQuizEl.addEventListener('click', function() {
-   // timeLeft === 60;
-   // console.log("Start game");
-   // });
+var timeLeft = ""
+   startQuizEl.addEventListener('click', function() {
+   countdownTimer();  
+   timeLeft === 60;
+   console.log("Start game");
+   });
+
+function countdownTimer() {
+   var timeLeft = 60
+   var timeInterval = setInterval(function() {
+      timeLeft--; 
+        countdownEl.textContent = timeLeft;
+        if (timeLeft === 0) { 
+        clearInterval(timeInterval); 
+      }
+   }, 1000);
+};   
+
+// GAME ENDS IF COUNTDOWNTIMER = 0
 
 
-// THIS CODE WORKS
-// function countdownTimer() {
-   // 
-   // var timeLeft = 60
-   // var timeInterval = setInterval(function() {
-      // timeLeft--; 
-      // countdownEl.textContent = timeLeft;
-      // if (timeLeft === 0) { 
-         // clearInterval(timeInterval); 
-         // }
-      // }, 1000);
-// };   
-// countdownTimer();
-// console.log("timer is counting");
+
 
 // QUESTIONS SECTION
+//
 
- questionEl.textContent
+ //questionEl.textContent
 
 
 // ENTER INITIALS AT END OF GAME
