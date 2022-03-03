940166a52f78f370354d77e6c3b86bd4ae26b47f
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-03-03 13:57:28 -0600

    hide start button and style response line

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,60 +1,141 @@
+// start button and timer
 var startQuizEl = document.getElementById('start-game');
 var countdownEl = document.getElementById('countdown');
-
+// quiz area
+var quizAreaEl = document.getElementById('quiz-area')
 // questions
 var questionEl = document.getElementById('questions');
 // answers
-var answerAEl = document.getElementById('ansA')
-var answerBEl = document.getElementById('ansB')
-var answerCEl = document.getElementById('ansC')
-var answerDEl = document.getElementById('ansD')
+var answerAEl = document.getElementById('ansA');
+var answerBEl = document.getElementById('ansB');
+var answerCEl = document.getElementById('ansC');
+var answerDEl = document.getElementById('ansD');
+// correct answers
+//var 
+
+// answer response
+var answerRespondEl = document.getElementById('answer-respond');
+
+// score
+var scoreEl = document.getElementById('high-score');
+
+// question array
+var questionArr =[
+   {  questionEl : "This is the first question", 
+      answerAEl : "A. This is answer A", 
+      answerBEl : "B. This is answer B", 
+      answerCEl : "C. This is answer C", 
+      answerDEl : "D. This is answer D"
+      
+   },
+   {  questionEl : "This is the second question", 
+      answerAEl : "A. This is answer A", 
+      answerBEl : "B. This is answer B", 
+      answerCEl : "C. This is answer C", 
+      answerDEl : "D. This is answer D"
+      
+   },
+];
+// answer array
+var answerArray = [answerAEl, answerDEl];
+
 
 
 // CLICK TO START GAME
-var timeLeft = ""
+var timeLeft = "";
 startQuizEl.addEventListener('click', function() {
    countdownTimer();  
-// NEED TO CALL QUIZ ARRAY FUNCTION();
+// !!!! quizLaunch();
    // timeLeft === 60; (don't need this?)
    console.log("Start game");
+
+   // hide start button
+   startQuizEl.setAttribute("class", "hidden");
+   // unhide quiz 
+   quizAreaEl.removeAttribute('class', 'hidden');
 });
 
 // COUNTDOWN TIMER
 function countdownTimer() {
-   var timeLeft = 10
+   var timeLeft = 5
    var timeInterval = setInterval(function() {
       timeLeft--; 
       countdownEl.textContent = timeLeft;
 
       if (timeLeft === 0) { 
       clearInterval(timeInterval); 
-      countdownEl.textContent = "Times Up!";   
+      quizAreaEl.setAttribute('class', 'hidden');
+      countdownEl.textContent = "Times Up!";  
+      answerRespondEl.innerHTML = "<p>" + "You ran out of time!" + "<br>" + "That's OK, keep practicing and try again later! " + "</p>";
+
+
+      
+      // !!! STOP QUESTION FOR LOOP
+
       } 
    }, 1000);
 };   
 
+
+
 // !!!! GAME ENDS IF COUNTDOWNTIMER = 0
+
+// !!! START BUTTON DISAPPEARS AFTER GAME STARTS
 
 
 // QUESTIONS SECTION 
-// CREATE QUESTION ARRAY
 
-var quizQuestionsArr = [q1, q2, q3, q4, q5];
 
-var q1 = [
+
+
+// run quiz
+// var questionLoop = function() {
+   // for (let i = 0; i < questionArr.length; i++) {
+      // 
+   // } 
+// };
+
+// scroll through questions
+// var quizLaunch = function () {
+   // var i = 1
+   // let currentQuestion = questionArr[i];
+// 
+      // questionEl.innerHTML = "<h2>" + currentQuestion.questionEl + "</h2>";
+      // answerAEl.innerHTML = "<label>" + currentQuestion.answerAEl + "</label>";
+      // answerBEl.innerHTML = "<label>" + currentQuestion.answerBEl + "</label>";
+      // answerCEl.innerHTML = "<label>" + currentQuestion.answerCEl + "</label>";
+      // answerDEl.innerHTML = "<label>" + currentQuestion.answerDEl + "</label>";
+// };
+// quizLaunch();
+
+
+var i = 0
+var currentQuestion = questionArr[i];
+var questionArr = [questionEl.innerHTML = "<h2>" + currentQuestion.questionEl + "</h2>",
+                     answerAEl.innerHTML = "<label>" + currentQuestion.answerAEl + "</label>",
+                     answerBEl.innerHTML = "<label>" + currentQuestion.answerBEl + "</label>",
+                     answerCEl.innerHTML = "<label>" + currentQuestion.answerCEl + "</label>",
+                     answerDEl.innerHTML = "<label>" + currentQuestion.answerDEl + "</label>" ];
+
+                     
+// .addEventListener('click', function() {
+   // console.log('click');
+// };
+// 
+
+//  IF ANSWER IS WRONG, DEDUCT TIME
+//  timeLeft = timeLeft - 5
+//  "That is incorrect"
+
+
+
+
+// IF ANSWER IS RIGHT
    
-   "This is the first question", "A. This is answer A", "This is answer B", "This is answer C", "This is answer D",]
+    var score = 0;
+    score += 10;
 
-// ask question
-var askQuestions = function() {
-   for (let i = 0; i < quizQuestionsArr.length; i++) {
-      
-   };
-   
-}
-
-
-
+//  "You are correct!"
 
 
 
@@ -67,5 +148,4 @@ var askQuestions = function() {
 
 
 
-// ENTER INITIALS AT END OF GAME
- 
+// ENTER INITIALS AT END OF GAME -- localStorage
